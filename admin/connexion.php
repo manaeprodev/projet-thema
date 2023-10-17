7629be4c635cd28ca28a8e7b5cf60a3389ae42d9
Tony TIRYAKI <ttiryaki@mediametrie.fr>
2023-10-17 16:36:43 +0200

    add bdd connection

---
+++ admin/connexion.php
@@ -0,0 +1,15 @@
+<?php
+// Informations de connexion à la base de données
+$serveur = getenv('SERVER');
+$nomUtilisateur = getenv('USER_BDD');
+$motDePasse = getenv('PASSWORD_BDD');
+$baseDeDonnees = getenv('BDD_NAME');
+
+// Établir la connexion à la base de données
+$connexion = new mysqli($serveur, $nomUtilisateur, $motDePasse, $baseDeDonnees,3306);
+
+// Vérification des erreurs de connexion
+if ($connexion->connect_error) {
+    die("Erreur de connexion à la base de données : " . $connexion->connect_error);
+}
+?>
