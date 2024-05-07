6fe431373f26e9a82671005f4806546131c07b5b
Tony TIRYAKI <ttiryaki@mediametrie.fr>
2024-05-07 09:40:39 +0200

    send mails

--- admin/jobs/mailing/playNotification/notify.php
+++ admin/jobs/mailing/playNotification/notify.php
@@ -1,45 +1,32 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
 
-require '../../../../vendor/autoload.php'; // Assurez-vous d'avoir PHPMailer installé via Composer
+require '../../../../vendor/autoload.php';
 require '../../../../components/connexion.php';
 
-$mail = new PHPMailer(true);
+$htmlBody = file_get_contents('template.html');
 
-try {
-    $requete = "SELECT DISTINCT email FROM users WHERE wants_emails = 1";
-    $userData = array();
-    $stmt = $connexion->prepare($requete);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    // Configurations SMTP pour Gmail
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'contactpredeect@gmail.com'; // Remplacez par votre adresse Gmail
-    $mail->Password = 'LApinutella2802-_'; // Remplacez par votre mot de passe d'application
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // Utilisez TLS si nécessaire
-    $mail->Port = 993; // Ou 587 pour TLS
+$transport = (new Swift_SmtpTransport('smtp.gmail.com', 587, 'tls'))
+    ->setUsername('contactpredeect@gmail.com')
+    ->setPassword('twpesejjnruponjq');
 
-    // Paramètres de l'email
-    $mail->setFrom('no-reply-predeect@gmail.com', 'PREDEECTA via totodede.nvsphr.fr');
-    while ($row = $result->fetch_assoc()) {
-        $mail->addBCC($row['email']);
-    }
-    $mail->Subject = 'Un tirage est prévu ce soir! Venez prédire avant 17:00.';
+$requete = "SELECT DISTINCT email FROM users WHERE wants_emails = 1";
+$stmt = $connexion->prepare($requete);
+$stmt->execute();
+$result = $stmt->get_result();
 
-    // Charger le template HTML
-    $templatePath = 'template.html'; // Chemin vers votre fichier HTML
-    $htmlBody = file_get_contents($templatePath); // Charger le contenu du fichier
+$emails = [];
 
-    $mail->isHTML(true); // Indique que le corps est en HTML
-    $mail->Body = $htmlBody; // Utiliser le template HTML
-
-    // Envoyer l'email
-    $mail->send();
-    $stmt->close();
-    echo 'Email envoyé avec succès';
-} catch (Exception $e) {
-    echo 'L\'email n\'a pas pu être envoyé. Erreur : ', $mail->ErrorInfo;
+while ($row = $result->fetch_assoc()) {
+    $emails[] = $row['email'];
 }
+
+$mailer = new Swift_Mailer($transport);
+
+$message = (new Swift_Message('Un tirage est prévu ce soir! Venez prédire dés maintenant!'))
+    ->setFrom(['contactpredeect@gmail.com' => 'Tony Tiryaki de PREDEECT'])
+    ->setBcc($emails)
+    ->setBody($htmlBody, 'text/html');
+
+$result = $mailer->send($message);
+
+echo 'Email envoyé avec succès';
